87d95f9ed72c7c0ba271ca1f675dad57ff4febda
joealmond <jozsef.mandula@gmail.com>
2024-08-26 15:51:02 +0200

    fixed adding postHtml as content in createPost

--- src/js/createPost.js
+++ src/js/createPost.js
@@ -3,17 +3,24 @@ import path from "path";
 import matter from "gray-matter";
 
 import createPostHtml from "./createPostHtml.js";
+import createBaseHtml from "../createBaseHtml.js";
 
-export default function createPost(directory, filename, config, layouts) {
+export default async function createPost(directory, filename, config, layouts) {
   try {
     const fileContent = fs.readFileSync(path.join(directory, filename), "utf8");
     const { data: metadata, content } = matter(fileContent);
-    const postHtml = createPostHtml(
+    const postHtml = await createPostHtml(
       content,
       metadata
     );
+    const postWithBaseHtml = createBaseHtml(
+      config,
+      postHtml,
+      filename,
+      layouts
+    );
     console.log(`Generated post from ${filename}`);
-    return postHtml;
+    return postWithBaseHtml;
   } catch (error) {
     console.error(
       `Error occurred while importing or processing the file ${filename}:`,
