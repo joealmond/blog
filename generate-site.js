0110c63cc4fb06b875bc3653bb9c08c1939edb64
joealmond <jozsef.mandula@gmail.com>
2024-08-26 10:59:07 +0200

    site generation stops on error

--- generate-site.js
+++ generate-site.js
@@ -4,24 +4,45 @@ import createPosts from "./src/js/createPosts.js";
 import createLayouts from "./src/js/createLayouts.js";
 import copyAllFiles from "./src/js/copyAllFiles.js";
 
-console.log("\nGenerating site...\n");
-
-const config = configLoader();
-config.build.rootPath = import.meta.dirname;
-
-copyAllFiles(config);
-
-const layouts = await createLayouts(config);
-
-await createPosts(config, layouts);
-await createPages(config, layouts);
-
-console.log("\nSite generated successfully!\n");
-
-
-
 //TODO: add custom css and js, meta tag, title, etc.
 //TODO: syntax highlighting
 //TODO: system for tagging and categories, and search, linking between posts, pagination
 //TODO: interaction with posts, comments, likes, etc.
-//TODO: drafts
+//TODO: can mark drafts
+
+generateSite();
+
+async function generateSite() {
+  console.log("\nGenerating site...\n");
+
+  const config = configLoader();
+  if (!config) {
+    console.error("Error loading configuration. Aborting site generation.");
+    return;
+  }
+
+  config.build.rootPath = import.meta.dirname;
+
+  if (!copyAllFiles(config)) {
+    console.error("Error copying files. Aborting site generation.");
+    return;
+  }
+
+  const layouts = await createLayouts(config);
+  if (!layouts) {
+    console.error("Error creating layouts. Aborting site generation.");
+    return;
+  }
+
+  if (!(await createPosts(config, layouts))) {
+    console.error("Error creating posts. Aborting site generation.");
+    return;
+  }
+
+  if (!(await createPages(config, layouts))) {
+    console.error("Error creating pages. Aborting site generation.");
+    return;
+  }
+
+  console.log("\nSite generated successfully!\n");
+}
