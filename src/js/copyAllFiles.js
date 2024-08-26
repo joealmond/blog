aa6a25ce0bcf8fd6f4cf1519ae200076355c30da
joealmond <jozsef.mandula@gmail.com>
2024-08-26 10:02:37 +0200

    added error handling to copyAllFiles

--- src/js/copyAllFiles.js
+++ src/js/copyAllFiles.js
@@ -1,18 +1,25 @@
 import path from "path";
-
 import { copyFiles } from "./utility.js";
 
 export default function copyAllFiles(config) {
-  const { rootPath } = config.build;
+  try {
+    const { rootPath } = config.build;
     const { toCopy } = config;
+
     if (!toCopy || toCopy.length === 0) {
       console.log("No files to copy.");
       return;
     }
+
     toCopy.forEach((directory) => {
       const { srcDir, destDir } = directory;
-      copyFiles(path.join(rootPath,srcDir), path.join(rootPath,destDir));
+      copyFiles(path.join(rootPath, srcDir), path.join(rootPath, destDir));
     });
-  };
 
-
+    console.log("All files copied successfully.");
+    return true;
+  } catch (error) {
+    console.error("An error occurred while copying files:", error);
+    return false;
+  }
+}
